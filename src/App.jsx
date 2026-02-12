727e6abd2fc3b3069ca0e31aaded9acde0aca576
Fred Okech <89655979+fredokaych@users.noreply.github.com>
2026-02-12 13:38:58 +0300

    Analytics integration

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,32 @@ import Home from "./pages/Home";
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Lecturers2007 from "./pages/Lecturers2007";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import ReactGA from "react-ga4";
+
+function AnalyticsTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.initialize("G-HLEKES7WWM");
+  }, []);
+
+  useEffect(() => {
+    ReactGA.send({
+      hitType: "pageview",
+      page: location.pathname + location.search,
+    });
+  }, [location]);
+
+  return null;
+}
 
 export default function App() {
   return (
     <>
       <Header />
+      <AnalyticsTracker />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/57-lecturers" element={<Lecturers2007 />} />
